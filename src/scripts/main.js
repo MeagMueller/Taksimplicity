4eecfe14f21dfb1f4575d9dae6557e0ea12c64b8
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-05-03 10:59:19 -0500

    Building new performance form

---
+++ src/scripts/main.js
@@ -0,0 +1,3 @@
+import performances from "./performances/performances"
+
+performances.createNewPerformance()
--- src/scripts/performances/performances.js
+++ src/scripts/performances/performances.js
@@ -1,2 +1,34 @@
 import fetchCalls from "../apiManager"
 
+const displayContainer = document.querySelector("#display-container")
+
+const performances = {
+    createNewPerformance() {
+
+        let newPerformanceFragment = document.createDocumentFragment()
+
+        let newPerformanceLabel = document.createElement("label")
+        newPerformanceLabel.textContent = "New Performance: "
+        newPerformanceFragment.appendChild(newPerformanceLabel)
+
+        let newPerformanceInput = document.createElement("input")
+        newPerformanceInput.setAttribute = ("id", "newPerformanceInput")
+        newPerformanceInput.placeholder = "Name"
+        newPerformanceFragment.appendChild(newPerformanceInput)
+
+        let newPerformanceLocationLabel = document.createElement("label")
+        newPerformanceLocationLabel.textContent = "Location: "
+        newPerformanceFragment.appendChild(newPerformanceLocationLabel)
+
+        let newPerformanceLocationInput = document.createElement("input")
+        newPerformanceLocationInput.setAttribute = ("id", "newPerformanceLabelInput")
+        newPerformanceLocationInput.placeholder = "Address"
+        newPerformanceFragment.appendChild(newPerformanceLocationInput)
+
+
+        displayContainer.appendChild(newPerformanceFragment)
+
+    }
+}
+
+export default performances
