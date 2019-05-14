0842fce471d86d45ada064e6c61ea2fc6ecb9e8b
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-05-14 12:27:41 -0500

    C on props down

--- src/scripts/props/propsList.js
+++ src/scripts/props/propsList.js
@@ -8,10 +8,52 @@ const listProps = {
 
         fetchCalls.getAllProps().then(propsResponse => {
             console.log(propsResponse)
+
+            let listPropsFragment = document.createDocumentFragment()
+
+            let propsDiv = document.createElement("div")
+            propsDiv.setAttribute("id", "propsDiv")
+
+            let createNewPropButtonListed = document.createElement("button")
+            createNewPropButtonListed.textContent = "Add New Prop"
+            createNewPropButtonListed.addEventListener("click", () => {
+                props.createNewProp()
+            })
+
+            propsDiv.appendChild(createNewPropButtonListed)
+
+            propsResponse.forEach((propsResponse) => {
+                let propsResponseName = document.createElement("h2")
+                propsResponseName.textContent = propsResponse.name
+
+                let propsResponseDanceStyles = document.createElement("p")
+                propsResponseDanceStyles.textContent = propsResponse.danceStyle.name
+
+                listPropsFragment.appendChild(propsResponseName)
+                listPropsFragment.appendChild(propsResponseDanceStyles)
+
+                // let propsEditButton = document.createElement("button")
+                // propsEditButton.setAttribute("id", `propEditButton_${propsResponse.id}`)
+                // propsEditButton.textcontent = "Edit Prop"
+                // propsEditButton.addEventListener("click",)
+
+                let eachPropContainer = document.createElement("div")
+                eachPropContainer.setAttribute("id", `eachPropDiv_${propsResponse.id}`)
+                eachPropContainer.appendChild(listPropsFragment)
+
+                propsDiv.appendChild(eachPropContainer)
+            })
+
+            let propsSection = document.querySelector("#display-props")
+
+            while(propsSection.firstChild) {
+                propsSection.removeChild(propsSection.firstChild)
+            }
+
+            propsSection.appendChild(propsDiv)
         })
 
     }
-
 }
 
 export default listProps
