ab2345ef38aeeb94b713ee5d3681a20033ba17eb
MeagMueller <46437198+MeagMueller@users.noreply.github.com>
2019-05-15 23:00:48 -0500

    Can log out, just need session storage clear to cooperate!

--- src/scripts/landingPage.js
+++ src/scripts/landingPage.js
@@ -38,7 +38,6 @@ const landingPage = {
 
         landingPageContainer.appendChild(landingPageLoginDiv)
 
-        
         }
     }
 
--- src/scripts/logout/logout.js
+++ src/scripts/logout/logout.js
@@ -1,5 +1,6 @@
 import loginHandlers from "../login/loginHandlers"
 import login from "../login/login"
+import landingPage from "../landingPage";
 
 const logoutContainer = document.querySelector("#logout-container")
 const allContainer = document.querySelector("#all-container")
@@ -14,38 +15,49 @@ const logout = {
         logoutButton.setAttribute("id", "logoutButton")
         logoutButton.textContent = "Logout"
 
-        logoutButton.addEventListener("click", this.logoutHandler)
+        logoutButton.addEventListener("click", () => {
+            let logoutConfirm = confirm("Are you sure you want to log out?")
+            if (logoutConfirm === true) {
+                this.logoutHandler()
+                while (allContainer.firstChild) {
+                    allContainer.removeChild(allContainer.firstChild)
+                }
+            }
+        })
 
         // FOR THE MORNING: why is removeItem removing the ID when the function hasn't even been called yet? Does it HAVE to be in an IF statement? 
 
         logoutButtonDiv.appendChild(logoutButton)
-        logoutContainer.appendChild(logoutButtonDiv)
+        allContainer.appendChild(logoutButtonDiv)
 
     },
 
     logoutHandler() {
 
+        let logoutContainerFragment = document.createDocumentFragment()
+
         let returnToAppDiv = document.createElement("div")
+        
 
         console.log("Logout Button Clicked")
 
         // sessionStorage.clear()
-            // if (logoutButton.clicked === true) {
-            //     sessionStorage.removeItem("userId")
-            // while (allContainer.firstChild) {
-            //     allContainer.removeChild(allContainer.firstChild)
-            // }
-        // }
-        
-        // console.log(sessionStorage.getItem("userId"))
 
-        // let returnToAppButton = document.createElement("button")
-        // returnToAppButton.setAttribute("id", "returnToApp")
-        // returnToAppButton.textContent = "Return to Taksimplicity"
+        let returnToAppButton = document.createElement("button")
+        returnToAppButton.setAttribute("id", "returnToAppButtom")
+        returnToAppButton.textContent = "Return to Taksimplicity"
+        logoutContainerFragment.appendChild(returnToAppButton)
 
-        // returnToAppButton.addEventListener("click", login.createLoginForm)
-        // returnToAppDiv.appendChild(returnToAppButton)
-        // logoutContainer.appendChild(returnToAppDiv)
+
+        returnToAppButton.addEventListener("click", () => {
+            landingPage.landingPageWithLogin()
+            while (logoutContainer.firstChild) {
+                logoutContainer.removeChild(logoutContainer.firstChild)
+            }
+        })
+
+        returnToAppDiv.appendChild(logoutContainerFragment)
+        logoutContainer.appendChild(returnToAppDiv)
     }
 
 }
